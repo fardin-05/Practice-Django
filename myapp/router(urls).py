3f5cc1e993c2673cb75caf92da15984fb8ecb20c
Fardin Khan <azimahmed9722@gmail.com>
2025-10-14 21:53:38 +0600

    Add files via upload

---
+++ myapp/router(urls).py
@@ -0,0 +1,9 @@
+
+from django.urls import path,include
+from rest_framework.routers import SimpleRouter
+from product.views import ProductViewSet, CategoryViewSet
+router = SimpleRouter()
+router.register('product', ProductViewSet)
+router.register('category', CategoryViewSet)
+
+urlpatterns = router.urls
