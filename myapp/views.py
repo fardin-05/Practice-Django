45f1b114870dfe4fb70983d9b3bf01e92bb45852
Fardin Khan <azimahmed9722@gmail.com>
2026-01-02 23:37:40 +0600

    commited

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Post
-
+#git
 def home (request):
     posts=Post.objects.all() 
     return render (request,'home.html',{'posts':posts})
